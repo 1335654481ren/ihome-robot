30105727b56fd159b1bb36bb6a59641bb045c799
1335654481ren <liangxiao.ren@qq.com>
2018-04-21 11:38:58 +0800

    add git error to auto_git.sh

--- auto-git.sh
+++ auto-git.sh
@@ -6,3 +6,28 @@ git config --global user.email "xiaoliang.ren@hobot.cc"
 git config --global user.name "xiaoliang.ren"
 git commit -m "add imu src"
 git push origin master
+
+git clone
+git init
+git config user.name .....
+git config user.email ......
+
+多账号登陆
+参考：
+1.https://www.jianshu.com/p/12badb7e6c10
+2.http://1024chan.com/2018/01/20/git-remote-permision/
+3.https://www.zhihu.com/question/46432334/answer/300371167
+
+renxl@ubuntu:~/.ssh$ cat config 
+Host github.com
+HostName github.com
+User git
+IdentityFile ~/.ssh/id_rsa
+Host github-1335654481ren
+HostName github.com
+User git
+IdentityFile ~/.ssh/id_rsa_mygit
+renxl@ubuntu:~/.ssh$ 
+
+cd ~/ihome-robot/
+git remote set-url origin github-1335654481ren:1335654481ren/web-socket.git
